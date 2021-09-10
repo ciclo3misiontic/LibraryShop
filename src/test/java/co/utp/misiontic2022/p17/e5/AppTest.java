c685ee8da6f13f481f1b6c23353db3bdcc628646
Jose Cabrera <cabrerajosemiguel28@gmail.com>
2021-09-10 14:32:06 -0500

    primera estructura

---
+++ src/test/java/co/utp/misiontic2022/p17/e5/AppTest.java
@@ -0,0 +1,20 @@
+package co.utp.misiontic2022.p17.e5;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
